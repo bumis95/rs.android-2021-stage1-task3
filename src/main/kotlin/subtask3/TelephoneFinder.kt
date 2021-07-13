f0c1ed6bd640b597473a6b9fb80c3495cd7ac7a7
bumis95 <bumis95@gmail.com>
2021-07-13 10:30:11 +0300

    Add tasks

--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -2,8 +2,35 @@ package subtask3
 
 class TelephoneFinder {
 
-    // TODO: Complete the following function
+    private val resultMas = mutableListOf<String>()
+
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+
+        if (number.matches("[0-9]*".toRegex())) {
+            number.forEachIndexed { index, char ->
+                when (char) {
+                    '0' -> replaceChars(index, number, listOf('8'))
+                    '1' -> replaceChars(index, number, listOf('2', '4'))
+                    '2' -> replaceChars(index, number, listOf('1', '3', '5'))
+                    '3' -> replaceChars(index, number, listOf('2', '6'))
+                    '4' -> replaceChars(index, number, listOf('1', '5', '7'))
+                    '5' -> replaceChars(index, number, listOf('2', '4', '6', '8'))
+                    '6' -> replaceChars(index, number, listOf('3', '5', '9'))
+                    '7' -> replaceChars(index, number, listOf('4', '8'))
+                    '8' -> replaceChars(index, number, listOf('5', '7', '9', '0'))
+                    else -> replaceChars(index, number, listOf('6', '8'))
+                }
+            }
+        } else return null
+
+        return resultMas.toTypedArray()
+    }
+
+    private fun replaceChars(index: Int, string: String, neighbors: List<Char>) {
+
+        for (element in neighbors) {
+            val str = string.replace(string[index], element)
+            resultMas.add(str)
+        }
     }
 }
